56afdbdf8ab0f980a2dabbcfb2b02ee289d0b732
Andremachado82 <andrels.machado82@gmail.com>
2021-07-16 23:56:41 -0300

    Get ById, Put e delete disponibilizado

--- src/main/java/com/crud/salareunioes/controllers/RoomController.java
+++ src/main/java/com/crud/salareunioes/controllers/RoomController.java
@@ -2,26 +2,34 @@ package com.crud.salareunioes.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.crud.salareunioes.exceptions.ResourceNotFoundException;
 import com.crud.salareunioes.models.Room;
 import com.crud.salareunioes.services.RoomService;
 
 @RestController
 @RequestMapping("/room")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RoomController {
 	
 	@Autowired
 	private RoomService roomService;
 	
 	@PostMapping
-	public ResponseEntity<Room> createRoom(@RequestBody Room room) {
+	public ResponseEntity<Room> createRoom(@Valid @RequestBody Room room) {
 		
 		return ResponseEntity.ok().body(roomService.createRoom(room));
 	}
@@ -30,5 +38,23 @@ public class RoomController {
 	public ResponseEntity<List<Room>> getListRoom() {
 		
 		return ResponseEntity.ok().body(roomService.getListRoom());
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Room> getByIdRoom(@PathVariable Long id) throws ResourceNotFoundException {
+		
+		return ResponseEntity.ok().body(roomService.getById(id));
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Room> updateRoom(@PathVariable Long id, @Valid @RequestBody Room room) throws ResourceNotFoundException {
+		
+		return ResponseEntity.ok().body(roomService.updateRoom(id, room));
+	}
+	
+	@DeleteMapping("{id}")
+	public void deleteById(@PathVariable Long id) throws ResourceNotFoundException {
+		
+		roomService.deleteById(id);
 	}
 }
--- src/main/java/com/crud/salareunioes/services/RoomService.java
+++ src/main/java/com/crud/salareunioes/services/RoomService.java
@@ -2,27 +2,56 @@ package com.crud.salareunioes.services;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.crud.salareunioes.exceptions.ResourceNotFoundException;
 import com.crud.salareunioes.models.Room;
 import com.crud.salareunioes.repositories.RoomRepository;
 
 @Service
 public class RoomService {
-	
+
 	@Autowired
 	private RoomRepository roomRepository;
-	
+
 	public Room createRoom(Room room) {
-		
+
 		return roomRepository.save(room);
-	} 
-	
+	}
+
 	public List<Room> getListRoom() {
 		List<Room> listRoom = roomRepository.findAll();
-		
+
 		return listRoom;
-	} 
+	}
+
+	public Room getById(Long id) throws ResourceNotFoundException {
+
+		return buscarRoomPorId(id);
+	}
+
+	public Room updateRoom(Long id, Room room) throws ResourceNotFoundException {
+		
+		Room roomExistente = buscarRoomPorId(id);;
+		
+		BeanUtils.copyProperties(room, roomExistente, "id");
+		
+		return roomRepository.save(room);
+	}
+	
+	public Room buscarRoomPorId(Long id) throws ResourceNotFoundException {
+		Room  roomExistente = roomRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Room.class.getName()));
+		return roomExistente;
+	}
+	
+	public void deleteById(Long id) throws ResourceNotFoundException {
+
+		buscarRoomPorId(id);
+		
+		roomRepository.deleteById(id);
+	}
 
 }
